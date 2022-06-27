0e48819c5999e2827d07e2836f9de2fac7c7ab22
Iquiji <yt.failerbot.3000@gmail.com>
2022-06-27 21:43:11 +0200

    more planning

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,1 +1,3 @@
+mod types;
+mod parse_nodes;
 // TODO:
---
+++ src/parser/types.rs
@@ -0,0 +1,17 @@
+// we need to save the amount of bytes needed to represent
+// different sized char,int,unsigned int
+// function pointer
+// union & struct
+// arrays
+// type qualifiers: const (C89), volatile (C89), restrict (C99) and _Atomic (C11) 
+pub struct CType{
+
+}
+
+// pub enum CTypeQualifier{
+//     None,
+//     Const,
+//     Volatile,
+//     Restrict,
+//     Atomic,
+// }
