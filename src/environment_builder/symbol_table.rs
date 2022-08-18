2927a6731d97352b59fc580b9aea6a193155c86a
Iquiji <yt.failerbot.3000@gmail.com>
2022-08-18 23:17:34 +0200

    back with new plans and ideas

---
+++ src/environment_builder/symbol_table.rs
@@ -0,0 +1,37 @@
+/*
+An identifier can denote:
+    object; 
+    a function; 
+    a tag or a member of a structure, union, or enumeration; 
+    a typedef name; 
+    a label name;
+*/
+/*
+different entities designated by the same identifier either have different scopes, or are in different name spaces.
+
+There are four kinds of scopes: 
+    - function 
+    - file
+    - block
+    - function prototype. (A function prototype is a declaration of a function that declares the types of its parameters.)
+*/
+
+/*
+label name is the only kind of identifier that has function scope.
+*/
+
+
+/*
+Structuring of Object in Scope:
+- ident
+- type
+- Assignment enumeration
+- completeness of type flag
+- qualifiers
+
+*/
+
+
+struct ScopeContainer{
+
+}
---
+++ src/mir/mod.rs
@@ -0,0 +1,7 @@
+/*
+Medium Level Intermediate Representation, gonna be emited by walking the AST in the environment_builder.
+Usage:
+- Emit Cranelift IR in Stage 1 of the compiler
+- Used in backend of Compiler and Stage 2
+
+*/
