ff2b70d2a70dfda8c416a43dbefb67933c1cf09f
Iquiji <yt.failerbot.3000@gmail.com>
2022-09-10 20:35:49 +0200

    cannot continue without proper type checking

--- C_Testfiles/dev_case/dev_case.c
+++ C_Testfiles/dev_case/dev_case.c
@@ -30,9 +30,15 @@ int factorial(int x){
     // printf("%d! => %d\n",x,res);
     return res;
 }
-
+int bored_malloc(int x){
+    malloc(1024000);
+    puts("malloc'd 1024B");
+    return bored_malloc(x + 1);
+}
 
 int main() {
+    puts("I was compiled by JankCC!");
+
     int a = 5;
     int b = 0;
     b = 1;
@@ -40,19 +46,20 @@ int main() {
     printf("a.addr=%#018x,a.val=%d\n",&a,a);
     printf("b.addr=%#018x,b.val=%d\n",&b,b);
 
-    long unsigned int malloc_addr = malloc(15);
+    long unsigned int malloc_addr = malloc(16);
 
-    puts("I was compiled by JankCC!");
     // printf("factorial %d: %d\n",0,factorial(0));
     // printf("factorial %d: %d\n",5,factorial(5));
     // printf("factorial %d: %d\n",12,factorial(12));
-    printf("malloc_addr: %#018x,%d \n",malloc_addr,0);
+    printf("malloc_addr: %#018x,stored_data: %d \n",malloc_addr,*malloc_addr);
     int* a_ptr = &a;
     a_ptr = a_ptr + 8;
     int* b_ptr = &b;
     b_ptr = b_ptr - 8;
     printf("a addr: %#018x,deref again: %d\n",a_ptr,*a_ptr);
-    printf("b addr: %#018x,deref again: %d\n",&b,*b_ptr);
+    printf("b addr: %#018x,deref again: %d\n",b_ptr,*b_ptr);
+
+    // bored_malloc(0);
 
     return 77;
 }
--- src/mir/instruction/mod.rs
+++ src/mir/instruction/mod.rs
@@ -15,6 +15,8 @@ pub(crate) enum MIRInstruction {
     GetAddrOfLocal(MIRValue,LocalRef),
     /// return value, input value, wanted type
     Deref(MIRValue,MIRValue,MIRType),
+    /// store location, input value, provided type
+    StoreAtAddr(MIRValue,MIRValue,MIRType),
     IntMath(MIRValue, MIRValue, MIRValue,IntMathKind),
     Compare(MIRValue,MIRValue,MIRValue),
     Call(MIRValue, String, Vec<MIRValue>, MIRSignature),
